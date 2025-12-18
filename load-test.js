3efb7fa3e8ae660c7e3b24f641c1ec1d9aef2b02
Cibur <cibur@MacBookAir.lan>
2025-12-18 15:02:25 +0530

    changes

--- load-test.js
+++ load-test.js
@@ -4,13 +4,13 @@ import { check, sleep, fail } from "k6";
 export const options = {
   stages: [
     { duration: "30s", target: 10 },
-    { duration: "30s", target: 50 },
-    { duration: "30s", target: 100 },
-    { duration: "30s", target: 300 },
-    { duration: "30s", target: 500 },
-    { duration: "30s", target: 1000 },
-    { duration: "30s", target: 2000 },
-    { duration: "30s", target: 5000 },
+    // { duration: "30s", target: 50 },
+    // { duration: "30s", target: 100 },
+    // { duration: "30s", target: 300 },
+    // { duration: "30s", target: 500 },
+    // { duration: "30s", target: 1000 },
+    // { duration: "30s", target: 2000 },
+    // { duration: "30s", target: 5000 },
   ],
   thresholds: {
     http_req_failed: ["rate<0.01"], // Abort if >1% failures
