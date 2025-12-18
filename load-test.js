1f5db366751e5794a7c42b2447b51eec93e38e3d
Akilan <129616422+AkilanS2003@users.noreply.github.com>
2025-12-18 20:17:09 +0530

    Update load test options for 500 users
    Uncomment the target for 500 users in load test options.

--- load-test.js
+++ load-test.js
@@ -3,11 +3,11 @@ import { check, sleep, fail } from "k6";
 
 export const options = {
   stages: [
-    { duration: "30s", target: 10 },
+    // { duration: "30s", target: 10 },
     // { duration: "30s", target: 50 },
     // { duration: "30s", target: 100 },
     // { duration: "30s", target: 300 },
-    // { duration: "30s", target: 500 },
+    { duration: "30s", target: 500 },
     // { duration: "30s", target: 1000 },
     // { duration: "30s", target: 2000 },
     // { duration: "30s", target: 5000 },
